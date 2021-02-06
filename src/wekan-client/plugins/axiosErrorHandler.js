a436c9bc862298da5c2820d6ff5299a1de3eda1f
Sam Walpole <walpoles93@googlemail.com>
2021-02-06 08:48:47 +0000

    Move client to separate repo

--- src/wekan-client/plugins/axiosErrorHandler.js
+++
@@ -1,9 +0,0 @@
-export default function ({ $axios, store }) {
-  $axios.onError(({ response }) => {
-    store.dispatch('snackbarQueue/addError', {
-      message: response.data.title,
-    })
-
-    return Promise.resolve(false)
-  })
-}
